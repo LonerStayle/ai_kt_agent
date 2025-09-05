a352d330c4e08d82096cfb2c0fd54902ac194b97
walker0625 <jeonminwoo0625@gmail.com>
2025-09-05 09:24:49 +0900

    feat : add redis settings

--- src/main.py
+++ src/main.py
@@ -5,9 +5,12 @@ from src.common.SelectImage import SelectImage
 from ollama import Client
 from src.tools.rag import hybrid_search, rerank
 import json
+import redis
+import uuid
 
 kt_base_name = "K-intelligence/Midm-2.0-Base-Instruct"
 client = Client(host="http://127.0.0.1:11434")
+cache = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
 
 # 어싱크로 감싼 이유는 추후 비동기를 사용하기 위함 입니다.
 # 툴 사용할때 LLM 호출 2번 할탠데 툴 불러오는 과정의 연산을 최적화 하기 위함 
@@ -40,6 +43,15 @@ def send_prompt(selected_places: list[SelectImage]):
 
     messages = prompts.build_question_prompts(selected_places,candidates)
     print(messages)
+    
+    # uuid라는 유니크한 문자열 생성
+    user_key = f"user:{uuid.uuid4().hex}"
+    print(f"user key: {user_key}")
+    
+    # redis(임시 저장소)에 생성한 값을 저장/불러오기
+    cache.set(user_key, json.dumps(messages), ex=300)  # 300초간 유지
+    print(cache.get(user_key))
+    
     for chunk in client.chat(
         model= gl.MODEL_NAME,  
         messages=messages,
@@ -51,7 +63,6 @@ def send_prompt(selected_places: list[SelectImage]):
         if isinstance(chunk, tuple): chunk = chunk[0]
         if "message" in chunk and "content" in chunk["message"]:
             print(chunk["message"]["content"], end="", flush=True)
-
 def main():
     send_prompt([SelectImage.A, SelectImage.B ])
 
