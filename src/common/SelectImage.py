3f8fd6f23ad7b8a3927a8094cc49ce6f15f13d4e
Loner <LonerStayle@github.com>
2025-09-07 10:51:11 +0900

    add SelectImage

--- src/common/SelectImage.py
+++ src/common/SelectImage.py
@@ -1,4 +1,9 @@
 from enum import Enum
 class SelectImage(Enum):
-    A = "A"
-    B = "B"
+    GYEONGBOKGUNG = "경복궁"
+    NAKSAN_WALL = "낙산공원 성곽길"
+    NAMSAN_TOWER = "남산 타워"
+    OLYMPIC_STADIUM = "서울 올림픽경기장"
+    COEX = "코엑스"
+    BUKCHON_HANOK = "북촌 한옥마을"
+    CHEONGDAM_BRIDGE = "청담대교"
