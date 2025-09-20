93493aa40ff7c34f8029478184090df64738d7da
Loner <LonerStayle@github.com>
2025-09-20 12:44:27 +0900

    add

--- src/tools/tavily_client.py
+++ src/tools/tavily_client.py
@@ -1,7 +1,6 @@
 from tavily import TavilyClient
 
 tavily_client = TavilyClient(api_key="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")
-# response = tavily_client.search("김치 좀 추천해줘")
 
 def tavily_chat(query: str, max_results=1) -> str:
     result = tavily_client.search(query, max_results=max_results)
